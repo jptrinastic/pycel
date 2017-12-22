9ba9df315e7a202391a0be203b73fdac6261680a
jptrinastic <jptrinastic@gmail.com>
2017-12-22 16:21:52 -0500

    Update excellib.py

--- src/pycel/excellib.py
+++ src/pycel/excellib.py
@@ -42,12 +42,18 @@ FUNCTION_MAP = {
       "gammaln" : "lgamma",
       "round" : "xround",
       "ceiling" : "xceil",
-      "mina": "xmina",
+      "mina" : "xmina",
+      "mech" : "xmech", 
       }
 
 ######################################################################################
 # List of excel equivalent functions
 # TODO: needs unit testing
+
+#---------------MACRO INPUTS-----------------#
+def xmech():
+    # Returns value based on Indices tab
+    return 0.0005
 
 def xmina(*args):
     # ignore non numeric cells
